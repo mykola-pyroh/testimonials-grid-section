16a6c77cf04c077bfd4563d5f8614d2a0f413f37
Mykola Pyroh <40759506+mykola-pyroh@users.noreply.github.com>
2025-01-19 00:29:36 +0200

    Changed card structure

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,11 +1,17 @@
+import './Card.scss';
+
 export default function Card({id, photo, name, status, title, comment}: {id: string, photo: string, name: string, status: string, title: string, comment: string}) {
     return (
         <article className={id}>
-            <img src={photo} alt={`${name} photo`} />
-            <p>{name}</p>
-            <p>{status}</p>
-            <h2>{title}</h2>
-            <p>{comment}</p>
+            <div className="userCard__userInfo">
+                <img src={photo} alt={`${name} photo`} className="userCard__userPhoto" />
+                <div className="userCard__userDetail">
+                    <p className="userCard__userName">{name}</p>
+                    <p className="userCard__userStatus">{status}</p>
+                </div>
+            </div>
+            <h2 className="userCard__userTitle">{title}</h2>
+            <p className="userCard__userComment">{comment}</p>
         </article>
     )
 }
