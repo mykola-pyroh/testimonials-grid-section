0df038d4a0859f21dd42e5f49622db5cc71cac1c
Mykola Pyroh <mykola.pyroh@ukr.net>
2025-01-18 20:12:07 +0200

    Added some Main structure and styles

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,10 +1,14 @@
 import Card from '../Card/Card';
 import data from '../../assets/data.json';
 
+import './Main.scss';
+
 export default function Main() {
     return (
-        <section>
-            {data.map(user => <Card key={user.name} photo={user.photo} name={user.name} status={user.status} title={user.title} comment={user.comment} />)}
-        </section>
+        <main>
+            <section className='cardsGrid'>
+                {data.map(user => <Card key={user.name} id={user.id} photo={user.photo} name={user.name} status={user.status} title={user.title} comment={user.comment} />)}
+            </section>
+        </main>
     )
 }
